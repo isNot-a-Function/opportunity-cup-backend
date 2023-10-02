dcb8688853d0d40ccccb3bf0a2b31fb3a22d1edb
Nikolay Kovalev <nick.max.kovaly@gmail.com>
2023-10-03 01:06:05 +0300

    fix: import

--- src/routes/executor.routes.ts
+++ src/routes/executor.routes.ts
@@ -1,5 +1,5 @@
 import { FastifyInstance } from 'fastify';
-import { UpdateExecutorInfoController } from 'controllers/executor/executor.controller';
+import { UpdateExecutorInfoController } from '../controllers/executor/executor.controller';
 
 export const executorRouter = (fastify: FastifyInstance, opts: any, next: (err?: Error) => void) => {
   fastify.post('/update', UpdateExecutorInfoController);
