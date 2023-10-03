9382211dfdf0e32703ae2acde4e5e917bd60ed57
Nikolay Kovalev <nick.max.kovaly@gmail.com>
2023-10-03 19:02:58 +0300

    fix: config

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,3 +6,9 @@ export const SERVER_PORT = process.env.SERVER_PORT || '5100';
 export const COOKIE_SECRET = process.env.COOKIE_SECRET || 'COOKIE_OC_SECRET';
 export const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET || 'JWT_REFRESH';
 export const JWT_ACCESS_SECRET = process.env.JWT_ACCESS_SECRET || 'JWT_ACCESS';
+
+export const SWIZE_AWS_BUCKET = process.env.SWIZE_AWS_BUCKET as string;
+export const SWIZE_AWS_ACCESS_KEY = process.env.SWIZE_AWS_ACCESS_KEY as string;
+export const SWIZE_AWS_SECRET_KEY = process.env.SWIZE_AWS_SECRET_KEY as string;
+export const SWIZE_AWS_REGION = process.env.SWIZE_AWS_REGION as string;
+export const SWIZE_SIGNED_URL_EXPIRES = process.env.SWIZE_SIGNED_URL_EXPIRES as string;
