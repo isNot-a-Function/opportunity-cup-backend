620f36c87c269ec13fd41bdd80cd66762e97dbb7
Nikolay Kovalev <nick.max.kovaly@gmail.com>
2023-10-03 23:53:16 +0300

    fix: aws

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,8 +7,8 @@ export const COOKIE_SECRET = process.env.COOKIE_SECRET || 'COOKIE_OC_SECRET';
 export const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET || 'JWT_REFRESH';
 export const JWT_ACCESS_SECRET = process.env.JWT_ACCESS_SECRET || 'JWT_ACCESS';
 
-export const SWIZE_AWS_BUCKET = process.env.SWIZE_AWS_BUCKET as string;
-export const SWIZE_AWS_ACCESS_KEY = process.env.SWIZE_AWS_ACCESS_KEY as string;
-export const SWIZE_AWS_SECRET_KEY = process.env.SWIZE_AWS_SECRET_KEY as string;
-export const SWIZE_AWS_REGION = process.env.SWIZE_AWS_REGION as string;
-export const SWIZE_SIGNED_URL_EXPIRES = process.env.SWIZE_SIGNED_URL_EXPIRES as string;
+export const SWIZE_AWS_BUCKET = process.env.SWIZE_AWS_BUCKET || 'inst-graphql-api';
+export const SWIZE_AWS_ACCESS_KEY = process.env.SWIZE_AWS_ACCESS_KEY || 'AKIAUGSVZHSW2NPFJ3VS';
+export const SWIZE_AWS_SECRET_KEY = process.env.SWIZE_AWS_SECRET_KEY || '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';
+export const SWIZE_AWS_REGION = process.env.SWIZE_AWS_REGION || 'eu-west-2';
+export const SWIZE_SIGNED_URL_EXPIRES = process.env.SWIZE_SIGNED_URL_EXPIRES || '3600';
