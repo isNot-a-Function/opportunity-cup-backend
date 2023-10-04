afedf4198a029cd7d09145f4432ee585c19ee6aa
Nikolay Kovalev <nick.max.kovaly@gmail.com>
2023-10-05 00:31:30 +0300

    fix

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,7 +2,7 @@ import { FastifyInstance } from 'fastify';
 import { AddLogoController, ChangeRoleController, GetUserController } from '../controllers/user/user.controller';
 
 export const userRouter = (fastify: FastifyInstance, opts: any, next: (err?: Error) => void) => {
-  fastify.get('/change', ChangeRoleController);
+  fastify.post('/change', ChangeRoleController);
 
   fastify.post('/logo', AddLogoController);
 
